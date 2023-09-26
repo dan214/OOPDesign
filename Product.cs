0bb7ddc4b5bc07acdfcb21fdde90642637409dda
Daniel Irungu <v-dairungu@microsoft.com>
2023-09-27 01:40:44 +0300

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDesign
+{
+    public class Product
+    {
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+
+        public int maxItemsInStock = 0;
+
+        private UnitType unitType;
+        private int amountInStock = 0;
+        private bool isBelowStockThreshold = false;
+
+        public void UseProduct(int items)
+        {
+            if(items <= amountInStock)
+            {
+                amountInStock -= items;
+
+                UpdateLowStock();
+
+                Log($"Amount in stock updated.Now {amountInStock} items in stock.");
+
+            }
+            else
+            {
+                Log($"Not enough items on stock for {CreateSimpleProductRepresentation()}.{amountInStock} available but {items} requested");
+            }
+        }
+        public void IncreaseStock()
+        {
+            amountInStock++;
+        }
+
+        public void UpdateLowStock()
+        {
+            if(amountInStock <= 10)
+            {
+                isBelowStockThreshold = true;
+            }
+        }
+        public string CreateSimpleProductRepresentation()
+        {
+            return $"Product {id} ({name})";
+        }
+        private void Log(string message)
+        {
+            Console.WriteLine (message);
+        }
+    }
+}
