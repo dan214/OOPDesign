eaa353f32f854ceebc1eb9ae47c4e3469f0f9312
Daniel Irungu <v-dairungu@microsoft.com>
2023-09-28 01:21:19 +0300

    add extra methods and properties

--- Product.cs
+++ Product.cs
@@ -8,42 +8,128 @@ namespace OOPDesign
 {
     public class Product
     {
+        public Product(int id, string name) 
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public Product(int id) : this(id, string.Empty)
+        {
+        }
+
+        public Product(int id, string name, string? description, UnitType unitType, int maxAmountInStock)
+        {
+            maxItemsInStock = maxAmountInStock;
+            UnitType = unitType;
+            Id = id;
+            Name = name;
+            Description = description;
+
+            UpdateLowStock();
+        }   
+
         private int id;
         private string name = string.Empty;
         private string? description;
 
-        public int maxItemsInStock = 0;
+        private int maxItemsInStock = 0;
 
-        private UnitType unitType;
-        private int amountInStock = 0;
-        private bool isBelowStockThreshold = false;
+        public UnitType UnitType { get; set; }
+
+        public int AmountInStock { get;private set; }
+
+        public bool IsBelowStockThreshold { get; private set; }
+
+        public int Id 
+        {
+            get { return id; }
+            set { id = value; }
+            
+        }
+        public string Name
+        {
+            get { return name; }
+            set 
+            { 
+                name = value.Length > 50 ? value[..50] : value;
+            }
+        }
+
+        public string? Description
+        {
+            get { return description; }
+            set
+            {
+                if(value == null)
+                {
+                    description = string.Empty;
+                }
+                else
+                {
+                    description = value.Length > 250 ? value[..250] : value;
+                }
+            }
+        }
 
         public void UseProduct(int items)
         {
-            if(items <= amountInStock)
+            if(items <= AmountInStock)
             {
-                amountInStock -= items;
+                AmountInStock -= items;
 
                 UpdateLowStock();
 
-                Log($"Amount in stock updated.Now {amountInStock} items in stock.");
+                Log($"Amount in stock updated.Now {AmountInStock} items in stock.");
 
             }
             else
             {
-                Log($"Not enough items on stock for {CreateSimpleProductRepresentation()}.{amountInStock} available but {items} requested");
+                Log($"Not enough items on stock for {CreateSimpleProductRepresentation()}.{AmountInStock} available but {items} requested");
             }
         }
         public void IncreaseStock()
         {
-            amountInStock++;
+            AmountInStock++;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            int newStock = AmountInStock + amount;
+
+            if( newStock < maxItemsInStock )
+            {
+                AmountInStock += amount;
+            }
+            else
+            {
+                AmountInStock = maxItemsInStock;
+                Log($"{CreateSimpleProductRepresentation} stock overflow. {newStock - AmountInStock} item(s) ordered that couldnt be stored.");
+            }
+
+            if(AmountInStock > 10)
+            {
+                IsBelowStockThreshold = false;
+            }
+        }
+
+        private void DecreaseStock(int items, string reason)
+        {
+            if(items <= AmountInStock)
+            {
+                AmountInStock -= items;
+            }
+            else
+            {
+                AmountInStock = 0;
+            }
         }
 
         public void UpdateLowStock()
         {
-            if(amountInStock <= 10)
+            if(AmountInStock <= 10)
             {
-                isBelowStockThreshold = true;
+                IsBelowStockThreshold = true;
             }
         }
         public string CreateSimpleProductRepresentation()
@@ -53,6 +139,37 @@ namespace OOPDesign
         private void Log(string message)
         {
             Console.WriteLine (message);
+        }
+
+        public string DisplayDetailsShort()
+        {
+            return $"{id}. {name} \n{AmountInStock} items in stock";
+        }
+        public string DisplayDetailsLong()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{id} {name} \n{description}\n{AmountInStock} item(s) in stock");
+
+            if(IsBelowStockThreshold)
+            {
+                sb.Append("\n!!STOCK LOW!!");
+            }
+
+            return sb.ToString();
+        }
+
+        public string DisplayDetailsLong(string extraDetails)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{id} {name} \n{description}\n{AmountInStock} item(s) in stock");
+            sb.Append (extraDetails);   
+
+            if (IsBelowStockThreshold)
+            {
+                sb.Append("\n!!STOCK LOW!!");
+            }
+
+            return sb.ToString();
         }
     }
 }
